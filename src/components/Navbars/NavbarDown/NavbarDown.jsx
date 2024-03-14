4f96575e3a3f991b3dcb0a373fc66cf624ff8411
WizarPoka <Vasya-nve12401@yandex.ru>
2024-03-15 00:33:16 +0300

    немного сделанный нижний navbar

--- src/components/Navbars/NavbarDown/NavbarDown.jsx
+++ src/components/Navbars/NavbarDown/NavbarDown.jsx
@@ -1,16 +1,42 @@
+// NavbarDown.jsx
 import React from 'react';
 import styles from './NavbarDown.module.css';
 
 function NavbarDown() {
   return (
     <div>
-      <div className={styles.navbarHomeDown}>
-        <p>
-          Выберите город 8(800)999-99-99
-          <span role="img" aria-label="Heart">❤️</span>
-          Корзина
-        </p>
+    <div className={styles.navbarDown}>
+      <div className={styles.logo}>
+        Logo
       </div>
+      <div className={styles.column}>
+        <div>О компании</div>
+        <div>Продукция</div>
+        <div>Доставка</div>
+        <div>Торты на заказ</div>
+      </div>
+      <div className={styles.column}>
+        <div>Торты на заказ</div>
+        <div>Акции</div>
+        <div>Работа в Пекарне №1</div>
+      </div>
+      <div className={styles.icons}>
+        <div>Инстаграм</div>
+        <div>Телеграм</div>
+        <div>ВКонтакте</div>
+        <div>example@gmail.com</div>
+      </div>
+      </div>
+      <div className={styles.navbarDown}>
+        <hr className={styles.divider} />
+      </div>
+      <div className={styles.navbarDown}>
+      <div className={styles.footer}>
+        <div>Политика конфиденциальности</div>
+        <div>Публичная оферта</div>
+        <div>© Пекарня №1 2023</div>
+      </div>
+    </div>
     </div>
   );
 }
