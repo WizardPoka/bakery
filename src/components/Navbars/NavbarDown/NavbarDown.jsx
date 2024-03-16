b39e1de5732166b4669ec057c031ec581e20207a
WizarPoka <Vasya-nve12401@yandex.ru>
2024-03-16 09:45:42 +0300

    конечная версия, без красивых картинок

--- src/components/Navbars/NavbarDown/NavbarDown.jsx
+++ src/components/Navbars/NavbarDown/NavbarDown.jsx
@@ -5,38 +5,36 @@ import styles from './NavbarDown.module.css';
 function NavbarDown() {
   return (
     <div>
-    <div className={styles.navbarDown}>
-      <div className={styles.logo}>
-        Logo
-      </div>
-      <div className={styles.column}>
-        <div>О компании</div>
-        <div>Продукция</div>
-        <div>Доставка</div>
-        <div>Торты на заказ</div>
-      </div>
-      <div className={styles.column}>
-        <div>Торты на заказ</div>
-        <div>Акции</div>
-        <div>Работа в Пекарне №1</div>
-      </div>
-      <div className={styles.icons}>
-        <div>Инстаграм</div>
-        <div>Телеграм</div>
-        <div>ВКонтакте</div>
-        <div>example@gmail.com</div>
-      </div>
+      <div className={styles.navbarDown}>
+        <div className={styles.logo}>
+          Logo
+        </div>
+        <div className={styles.column}>
+          <div>О компании</div>
+          <div>Продукция</div>
+          <div>Доставка</div>
+          <div>Торты на заказ</div>
+        </div>
+        <div className={styles.column}>
+          <div>Торты на заказ</div>
+          <div>Акции</div>
+          <div>Работа в Пекарне №1</div>
+        </div>
+        <div className={styles.column}>
+          <div>Инстаграм</div>
+          <div>Телеграм</div>
+          <div>ВКонтакте</div>
+          <div>example@gmail.com</div>
+        </div>
       </div>
       <div className={styles.navbarDown}>
         <hr className={styles.divider} />
       </div>
       <div className={styles.navbarDown}>
-      <div className={styles.footer}>
         <div>Политика конфиденциальности</div>
         <div>Публичная оферта</div>
         <div>© Пекарня №1 2023</div>
       </div>
-    </div>
     </div>
   );
 }
